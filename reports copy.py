72cf744305a34426cf8ac2535ab6bec8aaac8293
CLLCode Github <cll.code.github@gmail.com>
2023-07-28 15:33:09 -0500

    push728

--- reports copy.py
+++ reports copy.py
@@ -9,7 +9,7 @@ def generate(filename, title, additional_info, table_data):
     styles = getSampleStyleSheet()
     report = SimpleDocTemplate(filename)
     report_title = Paragraph(title, styles["h1"])
-    report_info = Paragraph(pdf, styles["BodyText"])
+    report_info = Paragraph(additional_info, styles["BodyText"])
     table_style = [('GRID', (0,0), (-1,-1), 1, colors.black),
                 ('FONTNAME', (0,0), (-1,0), 'Helvetica-Bold'),
                 ('ALIGN', (0,0), (-1,-1), 'CENTER')]
